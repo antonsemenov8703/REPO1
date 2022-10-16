6137b3f4f361e33b6ac65772276cb98d6a7c05df
Anton <anton-semenov@mail.ru>
2022-10-17 00:07:18 +0300

    25

---
+++ DZ/25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А и Б: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+
+double Exponent(int number, int degree)
+{
+    double result = 1;
+    if(B < 0)
+    {
+        Console.WriteLine("Было введено ненатуральное число.");
+    } 
+    else
+    {
+        for (int i = 0; i < degree; i++)
+        {
+            result = result * number;
+        }
+    }
+    return result;
+}
+Console.WriteLine(Exponent(A, B));
+// Пока при отрецательном значении степени выводится надпись, но кроме неё значение result=1
