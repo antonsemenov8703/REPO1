131c9189233795e3577419f32f96572c1b39ef2c
Anton <anton-semenov@mail.ru>
2022-10-18 01:27:57 +0300

    25

--- DZ/25/Program.cs
+++ DZ/25/Program.cs
@@ -3,25 +3,29 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.WriteLine("Введите число А и Б: ");
+Console.WriteLine("Введите число А: ");
 int A = Convert.ToInt32(Console.ReadLine());
 int B = Convert.ToInt32(Console.ReadLine());
 
+bool ValidNum(int num)
+{
+    if (num < 0)
+    {
+        Console.WriteLine("Введите натуральное число: ");
+        return false;
+    }
+    return true;
+}
 double Exponent(int number, int degree)
 {
     double result = 1;
-    if(B < 0)
+    for (int i = 0; i < degree; i++)
     {
-        Console.WriteLine("Было введено ненатуральное число.");
-    } 
-    else
-    {
-        for (int i = 0; i < degree; i++)
-        {
-            result = result * number;
-        }
+        result = result * number;
     }
     return result;
 }
-Console.WriteLine(Exponent(A, B));
-// Пока при отрецательном значении степени выводится надпись, но кроме неё значение result=1
+if (ValidNum(B)) Console.WriteLine($"Число {A} в степени {B} = {Exponent(A, B)}");
+
+
+
