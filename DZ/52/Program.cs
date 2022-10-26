e8e097110c3c4b6127e7ed8d477a05b35b0df2f1
Anton <anton-semenov@mail.ru>
2022-10-27 00:57:49 +0300

    ::

---
+++ DZ/52/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 52. 
+// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите высоту матрицы: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину матрицы: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[M, N];
+FillArray(array);
+PrintArray(array);
+PrintAverage(array);
+
+void PrintAverage (int [,] matr)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    double Sum = 0;
+    double Average = 0;
+    for (int j = 0; j < matr.GetLength(1)-1; j++)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            Sum += matr[i,j];
+        }
+        Average = Math.Round(Sum/matr.GetLength(0),1);
+        Sum = 0;
+        Console.Write(Average +"; " );
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Как убрать ; в конце?
